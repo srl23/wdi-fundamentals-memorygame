1e7305703a27119358d8ec8c701fc8c77d34959c
e5d1a7d721df28f4cc8f75c57c3c06eff7958d66 <stephen.linfield@comcast.net>
2020-01-22 16:52:13 -0500

    game logic further developed

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,33 @@
 const cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
+let score = 0;
 
-let cardOne = cards[0];
-let cardTwo = cards[2];
+//let cardOne = cards[0];
+//let cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+//cardsInPlay.push(cardOne);
+//cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		
-		alert('You have found a match!');
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert('You have found a match!');
+			score += 1;
+			cardsInPlay.pop();
+			cardsInPlay.pop();
+		}
+		else if (cardsInPlay[0] !== cardsInPlay[1]) {
+			alert('Sorry, please try again!');
+			cardsInPlay.pop();
+			cardsInPlay.pop();
+		} else {
+			//nothing should happen if there are no two cards in the array
+		}	
 	}
-	else {
-		alert('Sorry, please try again!')
-	}
+}
+
+function flipCard (cardID) {
+	cardsInPlay.push(cards[cardID]);
+	console.log('user flipped ' + cards[cardID] + '.');
+	checkForMatch();
 }
